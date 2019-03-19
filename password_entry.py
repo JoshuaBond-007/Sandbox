e781232d2912f6964b218b80210d7a546a0757cd
jc321763 <joshua.bond@my.jcu.edu.au>
2019-03-19 15:16:32 +1000

    +added password censor

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,14 @@
 """Joshua Bond"""
+
+
+def main():
+    MIN_LENGTH = 4
+    password = input("Enter your password:: ")
+    while len(password) < MIN_LENGTH:
+        password = input("Your password must be {} longer::".format(MIN_LENGTH - len(password)))
+    censored = ""
+    for i in range(len(password)):
+        censored += "*"
+
+    print(censored)
+main()
